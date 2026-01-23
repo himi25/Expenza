559f423c33f6d61da87535fed2930296f1407def
him08 <himanshivarshney25@gmail.com>
2026-01-23 14:13:05 +0530

    mongodb-connection

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const connectDb= async()=>{
+    try {
+        await mongoose.connect('mongodb://localhost:27017/Expenza')
+        console.log('mongodb connected !!')     
+    }catch(error){
+         console.error("MongoDB connection failed:", error.message);
+         process.exit(1);
+    }
+}
+
+module.exports=connectDb;
