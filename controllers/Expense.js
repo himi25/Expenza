e5a1fa0564a2964a77f21f864dcbb9f01b545ade
him08 <himanshivarshney25@gmail.com>
2026-01-28 18:16:14 +0530

    Add input validation for expense APIs

--- controllers/Expense.js
+++ controllers/Expense.js
@@ -1,8 +1,43 @@
 const Expense = require('../models/Expense')
+const currdate= new Date()
+console.log(currdate)
+const ALLOWED_CATEGORIES =[
+    'Inventory',
+    'Rent',
+  'Salary',
+  'Utilities',
+  'Transport',
+  'Misc'
+]
 const addExpense = async(req,res)=>{
 try{
+    
+    const {amount,category,date}= req.body
+
+    //converting the date to object for comparision
+    const expenseDate = new Date(date)
+    if(amount==null){
+        return res.status(400).json({message:'please enter the amount'})
+      }
+    if(amount< 0){
+        return res.status(400).json({message:'invalid amount'})
+    }
+    if(!category){
+        return res.status(400).json({message:'please select a category'})
+    }
+    if(!ALLOWED_CATEGORIES.includes(category)){
+        return res.status(400).json({message:'invalid category'})
+    }
+    if(!date){
+        return res.status(400).json({message:'please provide a date'})
+    }
+    //checking the date if it exceeds today's date 
+    if(expenseDate > currdate){
+        return res.status(400).json({message:'invalid date'})
+    }
+  
     const expense= await Expense.create(req.body)
-    res.status(200).json({message:'Expense added successfully',expense})
+    res.status(201).json({message:'Expense added successfully',expense})
     }
 catch(error){
 res.status(400).json({message:'Error adding expense',error:error.message})
@@ -10,8 +45,8 @@ res.status(400).json({message:'Error adding expense',error:error.message})
 }
 const showExpenses=async(req,res)=>{
     try{
-        const expense= await Expense.find()
-        res.status(200).json({message:'Expense is here',expense})
+        const expenses= await Expense.find()
+        res.status(200).json({message:'Expense is here',expenses})
     }
     catch(error){
         res.status(400).json({message:'Error getting the expense',error:error.message})
