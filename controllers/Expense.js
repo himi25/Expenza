4591a2b1b44e4f0d6237b5fd1cc881d306766d81
him08 <himanshivarshney25@gmail.com>
2026-01-28 17:09:52 +0530

    Implement get expense API

--- controllers/Expense.js
+++ controllers/Expense.js
@@ -8,5 +8,13 @@ catch(error){
 res.status(400).json({message:'Error adding expense',error:error.message})
 }
 }
-
-module.exports= {addExpense}
+const showExpenses=async(req,res)=>{
+    try{
+        const expense= await Expense.find()
+        res.status(200).json({message:'Expense is here',expense})
+    }
+    catch(error){
+        res.status(400).json({message:'Error getting the expense',error:error.message})
+    }
+}
+module.exports= {addExpense,showExpenses}
--- routes/expense.routes.js
+++ routes/expense.routes.js
@@ -2,8 +2,8 @@ const express=require('express')
 const router=express.Router()
 
 const {addExpense} = require('../controllers/Expense')
-
+const {showExpenses}=require('../controllers/Expense')
 router.post('/',addExpense)
-
+router.get('/',showExpenses)
 module.exports = router;
 
